3e74b31ec629499f346466c4100d0f048fb5489c
irlam <cirlam@gmail.com>
2026-09-23 11:20:32 +0100

    Fix SessionManager usage on help and training pages

--- help_pages/view_pwa_health.php
+++ help_pages/view_pwa_health.php
@@ -9,8 +9,8 @@ require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../includes/SessionManager.php';
 require_once __DIR__ . '/../includes/navbar.php';
 
-SessionManager::start();
-if (!SessionManager::isLoggedIn()) {
+$sessionManager = new SessionManager();
+if (!$sessionManager->isAuthenticated()) {
     header('Location: /login.php');
     exit;
 }
